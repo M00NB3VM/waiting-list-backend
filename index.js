6747a7f8dfcd0ed627984ef9fd42abd79a1d2d79
Saga <92476062+M00NB3VM@users.noreply.github.com>
2022-08-15 13:38:01 +0200

    Added url

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const knex = require("./db");
 
 const io = new Server(server, {
   cors: {
-    origin: ["https://admin.socket.io", "http://localhost:3000", "https://amritb.github.io/"],
+    origin: ["https://admin.socket.io", "http://localhost:3000", "https://amritb.github.io/", "https://git.heroku.com/cme-saga-waitinglist.git"],
     credentials: true
   },
 });
