351cb142363b4640c34d9ee3e7618d7bd50a2d58
Saga <92476062+M00NB3VM@users.noreply.github.com>
2022-08-15 13:41:44 +0200

    Update index.js

--- index.js
+++ index.js
@@ -9,8 +9,7 @@ const knex = require("./db");
 
 const io = new Server(server, {
   cors: {
-    origin: ["https://admin.socket.io", "http://localhost:3000", "https://amritb.github.io/", "https://git.heroku.com/cme-saga-waitinglist.git"],
-    credentials: true
+    origin: ["https://admin.socket.io", "http://localhost:3000", "https://amritb.github.io/", "https://git.heroku.com/cme-saga-waitinglist.git"]
   },
 });
 
